d407216f8b26332a817749752619650492adf30a
taeng <xodn0720@naver.com>
2024-04-25 19:42:44 +0900

    Spring Update

---
+++ taeng/src/main/java/Spring/taeng/TaengApplication.java
@@ -0,0 +1,13 @@
+package Spring.taeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaengApplication.class, args);
+	}
+
+}
