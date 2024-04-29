3d71f78d7ffe7c8a330932f9edc387ad52aaea8a
taeng <xodn0720@naver.com>
2024-04-29 20:09:06 +0900

    ㅇㅇㅇ

--- taeng/src/main/java/Spring/taeng/Question/Controller/QuestionController.java
+++ taeng/src/main/java/Spring/taeng/Question/Controller/QuestionController.java
@@ -1,13 +1,25 @@
 package Spring.taeng.Question.Controller;
 
+import Spring.taeng.Question.Entity.Question;
+import Spring.taeng.Question.Repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-    public String list(){
+    public String list(Model model){
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
         return "forums";
     }
+
 }
