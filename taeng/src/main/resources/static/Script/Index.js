3ef6e0e55cd21e7f4f94142bac547a757eeb543c
taeng <xodn0720@naver.com>
2024-05-13 11:26:20 +0900

    update

--- taeng/src/main/resources/static/Script/Index.js
+++
@@ -1,7 +0,0 @@
-const myDivs = document.querySelectorAll('.section_second');
-
-myDivs.forEach(div => {
-  div.addEventListener('mouseleave', function() {
-    div.style.backgroundColor = 'blue'; // 호버가 끝나면 다시 파란색으로 변경
-  });
-});
