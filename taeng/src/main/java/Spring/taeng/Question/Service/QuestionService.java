92e5a97d2eb3fb40c0aae917efe55325b7b702a0
taeng <xodn0720@naver.com>
2024-05-17 13:57:07 +0900

    Update

--- taeng/src/main/java/Spring/taeng/Question/Service/QuestionService.java
+++ taeng/src/main/java/Spring/taeng/Question/Service/QuestionService.java
@@ -1,11 +1,13 @@
 package Spring.taeng.Question.Service;
 
+import Spring.taeng.Question.DataNotFoundException;
 import Spring.taeng.Question.Entity.Question;
 import Spring.taeng.Question.Repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +16,14 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        }
+        else{
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
