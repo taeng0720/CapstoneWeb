1578f8852c08a00cb9c0d63bd94129e1610e76b4
taeng <xodn0720@naver.com>
2024-06-27 17:24:29 +0900

    update

--- taeng/src/main/java/Spring/taeng/Answer/Repository/AnswerRepositoy.java
+++
@@ -1,7 +0,0 @@
-package Spring.taeng.Answer.Repository;
-
-import Spring.taeng.Answer.Entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepositoy extends JpaRepository<Answer, Integer> {
-}
